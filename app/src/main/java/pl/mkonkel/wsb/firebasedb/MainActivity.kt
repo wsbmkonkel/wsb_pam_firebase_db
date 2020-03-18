b7b2d87bd39ec0b4568c0acf562023571ea61777
Michał Konkel <konkel.michal@gmail.com>
2020-03-18 23:28:44 +0100

    Adding sample instructions

--- app/src/main/java/pl/mkonkel/wsb/firebasedb/MainActivity.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasedb/MainActivity.kt
@@ -4,9 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+//    TODO: Add FirebaseDatabase Instance here
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//      TODO: Invoke listener created below
+
+//      TODO: Add some UI handling here
+//      setOnClickListener() on the button
+//      then inside this listener get text from editText
     }
+
+
+//    TODO: Add DatabaseListener
+//    Get child("childName") - first branch
+//    Get child("branchName") - get element in this branch
+//    addValueEventListener
+//
+//    onDataChane() will be invoked when any data will be modified in whole child branch.
+//    You can print changed data in the log using Timber.i(data)
+
+//    TODO: Add Helper method for adding "news" to the DB.
+//    child() - go to notes branch
+//    child() - go to element in branch (ec. User One notes).
+//    child() - value in branch
+//    setValue(givenValue)
+//
+//    You can additional add onSuccess and onFailureListener
 }
---
+++ app/src/main/java/pl/mkonkel/wsb/firebasedb/model/Note.kt
@@ -0,0 +1,3 @@
+package pl.mkonkel.wsb.firebasedb.model
+
+// TODO: Add Note model here

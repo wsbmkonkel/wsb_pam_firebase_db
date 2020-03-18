7dec005b10c0dc1480358ef01f9a6865dfcd2564
Michał Konkel <konkel.michal@gmail.com>
2020-03-18 21:51:53 +0100

    Sample config. Adding Timber Adding App

---
+++ app/src/main/java/pl/mkonkel/wsb/firebasedb/App.kt
@@ -0,0 +1,16 @@
+package pl.mkonkel.wsb.firebasedb
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+
+    private companion object {
+        const val TAG = "Application"
+    }
+}
